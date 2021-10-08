aa6ab18dcbd30423cc9dbf78f5e870a3e21e7789
Butterfly-L <butterfly.liaw@gmail.com>
2021-10-08 11:41:42 +0200

    [fix]add vue.config.js & fix tailwind settings

---
+++ precache-manifest.65b3ea800d596ffc983e7d998bbfec37.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f60ffa7ceb7adee2fced5b302adcf7",
+    "url": "index.html"
+  },
+  {
+    "revision": "898379a7ebc1702db4bc25a5c1d627e8",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "b5259eedab0dcee2847c",
+    "url": "static/css/app.9f5af35a.css"
+  },
+  {
+    "revision": "799f17c8c4d844f3f7c2",
+    "url": "static/css/chunk-4d2bea12.99f511d4.css"
+  },
+  {
+    "revision": "90864605359df3081de365f2133320de",
+    "url": "static/img/arrow.90864605.svg"
+  },
+  {
+    "revision": "2b293d269d78e599acf2b8d28ec64299",
+    "url": "static/img/logo.2b293d26.svg"
+  },
+  {
+    "revision": "b5259eedab0dcee2847c",
+    "url": "static/js/app.6b6904f4.js"
+  },
+  {
+    "revision": "0665a08d8b2151e97894",
+    "url": "static/js/chunk-2d20813a.74111a76.js"
+  },
+  {
+    "revision": "799f17c8c4d844f3f7c2",
+    "url": "static/js/chunk-4d2bea12.3646d0f1.js"
+  },
+  {
+    "revision": "040383fa010bf710757e",
+    "url": "static/js/chunk-vendors.e9998ed0.js"
+  }
+]);
